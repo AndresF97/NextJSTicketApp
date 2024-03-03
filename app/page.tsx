585719cbbdd81b0a2c6880a64f1e498debba761d
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2024-03-03 20:43:29 +0900

    Set front end will work on backend next

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-
+import TicketCard from "./(components)/TicketCard"
 
 export default function Home() {
   return (
-    <h1>Dashbaord</h1>
+   <div className="p-5">
+    {/* the 'lg'/'xl' class is media query that listens for a specific length of the page */}
+    <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+    <TicketCard />
+    <TicketCard />
+    <TicketCard />
+    <TicketCard />
+    </div>
+   </div>
   )
 }
